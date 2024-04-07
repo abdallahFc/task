bf2797c510e133cfc8b5e4e8d375788fb581996e
Abdallah Mohamed <84878301+abdallahFc@users.noreply.github.com>
2024-04-07 15:06:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
-function checkPalindrome() {
-    let userInput = prompt('Enter a string:');
-    let caseSensitive = confirm('Do you want to consider the case of the entered string?');
+function fetchUsers() {
+    return fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => {
+            if (!response.status === 200) {
+                throw new Error('Failed to fetch users');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.log('Error fetching users:', error);
+            return [];
+        });
+}
 
-    if (userInput) {
-        let result = isPalindrome(userInput, caseSensitive);
-        if (result) {
-            alert('Palindrome!');
-        } else {
-            alert('Not a palindrome.');
+async function fetchUserPosts(userId) {
+    try {
+        let response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch user posts');
         }
-    } else {
-        alert('Invalid input. Please enter a string.');
+        return await response.json();
+    } catch (error) {
+        console.log('Error fetching user posts:', error);
+        return [];
     }
 }
-function isPalindrome(str, sen) {
-    if (!sen) {
-        str = str.toLowerCase();
+
+async function displayUserTabs() {
+    try {
+        let users = await fetchUsers();
+        let userTabs = document.getElementById("userTabs");
+        users.forEach((user, index) => {
+            let button = document.createElement("button");
+            button.textContent = user.name;
+            userTabs.appendChild(button);
+            button.addEventListener("click", async () => {
+                let posts = await fetchUserPosts(user.id);
+                displayPostTitles(posts);
+            });
+            if (index === 0) {
+                button.click();
+            }
+        });
+    } catch (error) {
+        console.error('Error fetching users:', error);
     }
-    let reversedStr = '';
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversedStr += str[i];
-    }
-    return str === reversedStr;
 }
-checkPalindrome();
+
+
+function displayPostTitles(posts) {
+    let postTitles = document.getElementById("postTitles");
+    postTitles.innerHTML = "";
+    posts.forEach(post => {
+        let postCard = document.createElement("div");
+        postCard.classList.add("PostCard");
+        let title = document.createElement("p");
+        title.textContent = post.title;
+        postCard.appendChild(title);
+        postTitles.appendChild(postCard);
+    });
+}
+
+displayUserTabs();
+
+
